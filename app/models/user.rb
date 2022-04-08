21000fc3a69ddf5123cad60c3e056fc6c7089cfe
EC2 Default User <ec2-user@ip-172-31-44-152.ec2.internal>
2022-04-08 07:58:03 +0000

    [Start] 応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+  has_many :relationships, class_name: "Relationship", foreign_key: "follwer_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name:"Relationship", foreign_key: "follwed_id", dependent: :destroy
+  
+  has
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50}
   
